f42915efeda8d49540da8a4e9b64c2668299ddb9
Connor Kurland <cekurland@yahoo.com>
2019-11-12 19:51:50 -0800

    created base models for recipe, review and ingredient

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-"""
-class Suggestion(models.Model):
+class Recipe(models.Model):
     recipeName = models.CharField(max_length=60)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipeDescription = models.CharField(max_length=240)
+    recipeDescription = models.CharField(max_length=500)
     foodImage = models.ImageField(
         max_length=144,
         upload_to='uploads/%Y/%m/%d/')
@@ -16,9 +14,40 @@ class Suggestion(models.Model):
         ('', ''),
     )
     foodCategory = models.CharField(max_length=10, choices=foodCategories)#may have to change max_length to 1, look at models documentation
-    created_on = models.DateTimeField(auto_now_add=True)
+    recipeUrl = models.URLField(max_length=240,blank=True)
+    recipeDirections =  models.CharField(max_length=240, blank=True)
 
+    # Maybe add a date created item
+    # created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.author.username + " " + self.suggestion
-"""
+        return self.recipeName
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.CharField(max_length=500)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ratingOptions = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    )
+    rating = models.CharField(max_length=1, choices=ratingOptions)
+
+    # Maybe add a date created item
+    # created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username + " " + self.review
+
+class Ingredient(models.Model):
+    ingredient = models.CharField(max_length=60)
+    amount = models.CharField(max_length=30)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ingredient + " " + self.amount
+
+
