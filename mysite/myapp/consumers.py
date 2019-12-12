11a69f85d56ed8da168c69afbb1400219ac0e43c
Connor Kurland <cekurland@yahoo.com>
2019-12-12 10:45:38 -0800

    got usernames on chat

--- mysite/myapp/consumers.py
+++ mysite/myapp/consumers.py
@@ -1,16 +1,18 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
 import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        self.user = self.scope["user"]
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
-            self.channel_name
+            self.channel_name,
         )
 
         self.accept()
@@ -26,6 +28,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        message = str(self.user)+ ": " + message
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
@@ -42,5 +45,5 @@ class ChatConsumer(WebsocketConsumer):
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
         }))
