d8f3511d68b047ba95f883ad90cbe0c3b92bc2f2
Connor Kurland <cekurland@yahoo.com>
2019-12-14 12:43:02 -0800

    temp scroll

--- mysite/myapp/static/js/app.js
+++ mysite/myapp/static/js/app.js
@@ -2,4 +2,5 @@ $(document).foundation()
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 
+document.getElementById("chat-log").scrollTop = document.getElementById("chat-log").scrollHeight
 
